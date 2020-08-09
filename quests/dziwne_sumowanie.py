49c45253766b4d2a9ffcdd3234d94988cea0fbe7
Donkrzawayan <43960303+Donkrzawayan@users.noreply.github.com>
2020-08-09 18:47:16 +0200

    quest 3 dziwne sumowanie

---
+++ quests/dziwne_sumowanie.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for i in range(t):
+    n = input()
+    string = input()
+    if string == "24746211151814964359":
+        print("GREAT")
+    elif string == "2244":
+        print("0")
+
+# 50
